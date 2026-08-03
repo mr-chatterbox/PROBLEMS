1b95973ea1395e758946174a75591c8b5f7835b7
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-03 23:16:53 +0530

    Conditional and Loops

--- P003.cpp
+++ P003.cpp
@@ -2,8 +2,51 @@
 using namespace std;
 
 int main(){
-int n;
-cin >> n;
+   // int n;
+    //cin >> n;
 
-cout << "value of n is: " << n << endl;
+//cout << "value of n is: " << n << endl;
+
+//IF A IS +VE
+/*
+int a;
+
+cin >> a;
+
+if (a>0){
+    cout << "A is positive" << endl;
+}
+else{
+    cout << "A is negative" << endl;
+}
+
+
+int a,b;
+
+cin >> a >> b;
+
+cout << "value of a & b is: " << a << " " << b << endl;
+*/
+
+//int a,b;
+//a = cin.get();
+
+//cout << "value of a is: " << a << endl;
+/*
+int a,b;
+
+cout << "Enter the value of a " << endl;
+cin >> a;
+cout << "Enter the value of b " << endl;
+cin >> b;
+
+if (a>b){
+    cout << "A is greater" << endl;
+}
+if (b>a){
+    cout << "B is greater" << endl;
+}
+*/
+
+
 }
