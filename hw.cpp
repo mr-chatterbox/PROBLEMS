de318a4c39ceec7c7538ae98e93226648536faa4
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-04 20:59:17 +0530

    HW Q

---
+++ hw.cpp
@@ -0,0 +1,51 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+// 1st Q.
+/*
+    int a = 9;
+    if(a == 9){
+        cout << "NINEY" << " ";
+    }
+
+    if(a>0){
+        cout << "POSITIVE" << "";
+    }
+    else{
+        cout << "NEGATIVE";
+    }
+ */
+// 2nd Q.
+/*
+ int a = 2;
+ int b = a+1;
+
+ if ((a=3)==b){
+    cout<<a;
+ }
+ else
+ {
+    cout<<a+1;
+ }
+*/
+// 3rd Q.
+/*
+int a = 24;
+
+if(a>20){
+    cout << "Love" << " ";
+}
+else if(a == 24){
+    cout << "Lovely";
+}
+else{
+    cout << "Babbar";
+}
+cout<<a;
+*/
+// 4th Q.
+
+
+
+}
