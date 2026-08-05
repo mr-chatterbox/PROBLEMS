1b6ba7bfc6476b70ccdeb3317f0f58993ee5b72c
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-05 20:08:05 +0530

    PATTERNS

--- Patterns.cpp
+++ Patterns.cpp
@@ -114,6 +114,32 @@ while(row<=n){
 */
 
 //Pattern 7
+/*
+int n;
+cin >> n;
+ 
+int row = 1;
+
+while(row<=n){
+
+    int col = 1;
+    int value = row;
+    while (col<=row){
+        cout << value;
+        value = value + 1;
+        col = col + 1;
+    }
+    cout << endl;
+    row = row + 1;
+    }
+*/
+
+//Pattern 8
+int n;
+cin >> n;
+
+
+
 
 
 }
