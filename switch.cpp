842d45e0e0b95bf132dd8bcf27685fab893f71bb
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-09 22:52:50 +0530

    Switch Case

--- switch.cpp
+++ switch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-
+/*
     int num = 1;
 
     cout << endl;
@@ -16,6 +16,30 @@ int main(){
             
         default: cout << "Default" << endl;
             break;
+    }
+        cout << endl;
+
+        return 0;
+*/
+
+// we can also use a NESTED SWITCH
+
+int num = 1;
+
+    cout << endl;
+    switch( num ){
+
+        case 'A': cout << "First" << endl;
+            break;
+
+        case 1: switch (num){
+            case 1: cout << "First Again" << endl;
+            break;
+        }
+            break;
+            
+        default: cout << "Default" << endl;
+
     }
         cout << endl;
 
