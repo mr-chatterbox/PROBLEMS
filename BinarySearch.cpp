734a17dc1529c33adb163a8ff645b580895c7fcb
mr-chatterbox <shuzaizaan@gmail.com>
2026-08-13 23:50:03 +0530

    Binary Search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,7 +6,7 @@ int binarySearch (int arr[], int size, int key){
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end)/2;
+    int mid = start + (end - start)/2;
 
     while (start <= end){
 
@@ -19,7 +19,7 @@ int binarySearch (int arr[], int size, int key){
         else{
             end = mid - 1;
         }
-        mid = (start + end)/2;
+        mid = start + (end - start)/2;
     }
     return -1;
 }
