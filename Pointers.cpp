2ac98b0c29c2c901c22c55d868c86a212c1b4ae8
mr-chatterbox <shuzaizaan@gmail.com>
2026-09-11 19:47:16 +0530

    Pointers

--- Pointers.cpp
+++ Pointers.cpp
@@ -14,5 +14,16 @@ int main(){
     cout << "Address of ptr: " << ptr << endl;
     cout << "Value of ptr: " << *ptr << endl;
 
+    double d = 4.3;
+    double *ptr2 = &d;
+
+    cout << "Address of ptr2: " << ptr2 << endl;
+    cout << "Value of ptr2: " << *ptr2 << endl;
+
+    cout << "Size of int: " << sizeof(num) << endl;
+    cout << "Size of int pointer: " << sizeof(ptr) << endl;
+
+    
+
     return 0;
 }
